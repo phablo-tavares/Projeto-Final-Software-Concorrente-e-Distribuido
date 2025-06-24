3c12d635b7a855e8e2351887624f0f9720fba19a
Phablo <phablo.tavares1@gmail.com>
2025-06-24 12:36:36 -0300

    inital commit

---
+++ src/test/java/com/example/projeto_final_scd/ProjetoFinalScdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_final_scd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinalScdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
